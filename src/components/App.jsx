cb1c960fab9ff67a905063ada560b1edd274f119
Nitish Jha <nitishjha6519@gmail.com>
2022-01-31 13:57:47 +0530

    ReactJs counter app useState

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState}  from 'react';
+
+
+function App(){
+const [count, setCount] = useState(0);
+
+function handleClick(){
+  
+  setCount(count+1);
+}
+    return (
+
+      <div>
+        <h2>{count} </h2>
+        <button onClick={handleClick}> Counter + </button>
+  </div>
+    );
+}
+
+export default App;
